894420f3f8080b20bffff66567ba801a76b0d8da
Sean O'Daniels <sodaniels@gmail.com>
2024-10-10 16:29:12 -0400

    fix: added www. to musaria link.

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://musaria.com"
+          href="https://www.musaria.com"
           target="_blank"
           rel="noopener noreferrer"
         >
